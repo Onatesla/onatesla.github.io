3a3fdd693be1d0c824974ec928073d7690ec412a
Onatesla <73800811+Onatesla@users.noreply.github.com>
2022-06-16 22:34:50 +0200

    shop add

--- src/navigate/header.jsx
+++ src/navigate/header.jsx
@@ -26,6 +26,7 @@ overflow:hidden;}
 {color:white;
 font-family:poppins;
 }
+
 @media screen and (max-width:1380px){
   .nav-menu{display:none;}
   .nav{
@@ -36,6 +37,34 @@ font-family:poppins;
   }
 }
 `;
+const ShopLink = styled.div`background:#939597;
+height: 4rem;
+width:12vw;
+border-radius:12px;
+overflow:hidden;
+color:white;
+
+gap:1.2rem;
+box-shadow: 0.01rem 0.01rem 8px rgba(0,0,0,0.17);
+display:flex;
+position: fixed; top: 80px; 
+right: 2vw;
+.img{
+    height: 8vh;
+    flex: 2;
+    background-Color: #ececec;
+    padding-right: 0.8rem,
+}
+@media (max-width:440px){
+  width:14vw;
+  height:3rem;
+  right:84vw;
+  top:12vh;
+  .img{
+    height:6vh;
+    padding-right:1.2rem;
+  }
+}`;
 
 const url = 'https://i.ibb.co/hcNcrQp/Senza-titolo-1.png';
 function Navbar() {
@@ -49,7 +78,28 @@ function Navbar() {
             <li><Link to="/photogrid" className="nav-item">I nostri lavori</Link></li>
             <li><Link to="/businesscard" className="nav-item">Contattaci</Link></li>
             <li><Link to="/contact" className="nav-item">Preventivo</Link></li>
+
           </ul>
+          <a href="https://digital-photo-di-francesco-fontana.sumupstore.com">
+            <ShopLink>
+              <img
+                src="https://img.icons8.com/ios/100/undefined/online-shop-favorite.png"
+                alt="shop"
+                className="img"
+              />
+
+              <div style={{
+                flex: 3, display: 'flex', gap: '2rem', alignItems: 'center', justifyContents: 'center', alignText: 'center',
+              }}
+              >
+                <h1 style={{ fontSize: '14px' }}>
+                  Vai allo
+                  <br />
+                  <span style={{ fontSize: '20px' }}>Shop!</span>
+                </h1>
+              </div>
+            </ShopLink>
+          </a>
         </div>
         <Ham />
       </section>
